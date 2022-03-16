c2f078913a3bbc4b2b1539e894c9eca57cb8bf71
luismerinou <lmerinoulizarna@gmail.com>
2022-03-16 10:59:03 +0100

    add use of props

--- src/App.js
+++ src/App.js
@@ -2,22 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 
 
-const Hello = () => {
-  return (
-      <div>
-        <p>Hello world from Hello component!!</p>
-      </div>
-  )
+const Hello = (props) => { //React components must be capitalized
+    return (
+        <div>
+            <h2>Hello {props.name} you are {props.age} from hello component</h2>
+        </div>
+    )
 }
 
+/* Using a root element is not the only working option.
+   An array of components is also a valid solution:
+
+    return [
+            <h1>Greetings from App component</h1>
+            <Hello name="Jaime" age={age}/>
+            <Hello name={name} age={30 + 15}/>
+    ]
+ */
 
 function App() {
-  return (
-   <div>
-     <h1>Greetings from App component</h1>
-     <Hello />
-   </div>
-  );
+    const name = "Luis"
+    const age = 25
+    return (
+        <div>
+            <h1>Greetings from App component</h1>
+            <Hello name="Jaime" age={age}/>
+            <Hello name={name} age={30 + 15}/>
+        </div>
+    );
 }
 
 export default App;
